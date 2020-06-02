180e2791c47379453d6d058ea89d0c92a5d9eb86
Obinna Onyimadu <obinnaonyimadu@obinnas-mbp-3.home>
2020-06-02 22:10:16 +0100

    initial master push

---
+++ src/main/java/com/brownbag/news/servicediscovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,17 @@
+package com.brownbag.news.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
+	}
+
+}
